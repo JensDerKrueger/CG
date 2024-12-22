ff6837501eccf76f41fc780e972a1eee32031ac2
Jens Krüger <jens.krueger@uni-due.de>
2024-12-22 15:03:53 +0100

    add Emscripten support

--- 10_HelloGL/main.cpp
+++ 10_HelloGL/main.cpp
@@ -1,48 +1,63 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <GLEnv.h>
+#include <Mat4.h>
+
 #include <iostream>
 #include <sstream>
 
-#include <GLDebug.h>
-
-GLFWwindow* window;
 GLuint vbo;
 GLuint vao;
 GLuint program;
 
 // triangle vertex position data
 constexpr float triangle[] = {
-	 0.0f,  0.5f, 0.0f, // top
-	-0.5f, -0.5f, 0.0f,	// bottom left
-	 0.5f, -0.5f, 0.0f  // bottom right
+  0.0f,  0.5f, 0.0f, // top
+  -0.5f, -0.5f, 0.0f,  // bottom left
+  0.5f, -0.5f, 0.0f  // bottom right
 };
 
-// vertex shader code
 const GLchar* vertexShaderSource{
-  "#version 410 core\n"
-  "layout(location = 0) in vec3 vPos;\n"
-  "void main()\n"
-  "{\n"
-  "  gl_Position = vec4(vPos, 1.0);\n"
-  "}\0"};
-
-// fragment shader code
-const GLchar* fragmentShaderSource{
-  "#version 410 core\n"
-  "out vec4 fragColor;\n"
-  "void main()\n"
-  "{\n"
-  "  fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0);\n"
-  "}\0"};
-
-void init() {
-  const GLubyte* glVersion = glGetString(GL_VERSION);
-  const GLubyte* slVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-  std::cout << glVersion << std::endl;
-  std::cout << slVersion << std::endl;
+#ifdef __EMSCRIPTEN__
+R"(#version 300 es 
+in vec3 vPos;
+void main()
+{
+  gl_Position = vec4(vPos, 1.0);
 }
+)"
+#else
+R"(#version 410 core
+layout(location = 0) in vec3 vPos;
+void main()
+{
+  gl_Position = vec4(vPos, 1.0);
+}
+)"
+#endif
+};
 
-void draw() {
+const GLchar* fragmentShaderSource{
+#ifdef __EMSCRIPTEN__
+R"(#version 300 es
+precision mediump float;
+out vec4 fragColor;
+void main()
+{
+  fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0);
+}
+)"
+#else
+R"(#version 410 core
+out vec4 fragColor;
+void main()
+{
+  fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0);
+}
+)"
+#endif
+};
+
+
+void draw(void* arg=nullptr) {
   GL( glClearColor(0.0f, 0.0f, 0.0f, 1.0f) );
   GL( glClear(GL_COLOR_BUFFER_BIT) );
   
@@ -50,8 +65,6 @@ void draw() {
   GL( glUseProgram(program) );
   GL( glDrawArrays(GL_TRIANGLES, 0, 3) );
   GL( glBindVertexArray(0) );
-  
-  glfwSwapBuffers(window);
 }
 
 void setupShaders() {
@@ -60,20 +73,20 @@ void setupShaders() {
   GL( glShaderSource(vertexShader, 1, &vertexShaderSource, NULL) );
   GL( glCompileShader(vertexShader) );
   checkAndThrowShader(vertexShader);
-  
+
   // create the fragment shader
   GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   GL( glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL) );
   GL( glCompileShader(fragmentShader) );
   checkAndThrowShader(fragmentShader);
-  
+
   // link shaders into program
   program = glCreateProgram();
   GL( glAttachShader(program, vertexShader) );
   GL( glAttachShader(program, fragmentShader) );
   GL( glLinkProgram(program) );
   checkAndThrowProgram(program);
-  
+
   GL( glDeleteShader(vertexShader) );
   GL( glDeleteShader(fragmentShader) );
 }
@@ -82,15 +95,15 @@ void setupGeometry() {
   // define VAO for triangle
   GL( glGenVertexArrays(1, &vao) );
   GL( glBindVertexArray(vao) );
-  
+
   // upload vertex positions to VBO
   GL( glGenBuffers(1, &vbo) );
   GL( glBindBuffer(GL_ARRAY_BUFFER, vbo) );
   GL( glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW) );
-  
+
   GL( glEnableVertexAttribArray(0) );
   GL( glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0) );
-  
+
   GL( glBindVertexArray(0) );
 }
 
@@ -114,60 +127,40 @@ void checkShaderCompileStatus(GLuint shaderId) {
   }
 }
 
-void processInput(GLFWwindow* window) {
+#ifndef __EMSCRIPTEN__
+void keyCallback(GLFWwindow* window, int key, int scancode, int action,
+                 int mods) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void sizeCallback(GLFWwindow* window, int width, int height) {
   int w, h;
   glfwGetFramebufferSize(window, &w, &h);
   GL( glViewport(0, 0, w, h) );
 }
+#endif
 
 int main(int argc, char** argv) {
-	// initialize glfw and glew
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __EMSCRIPTEN__
+  GLEnv glEnv{800,600,1,"My First OpenGL Program",true,false,3,0,true};
+#else
+  GLEnv glEnv{800,600,1,"My First OpenGL Program",true,false,4,1,true};
+  glEnv.setKeyCallback(keyCallback);
+  glEnv.setResizeCallback(sizeCallback);
+#endif
 
-	window = glfwCreateWindow(800, 600, "Assignment 10 - Hello OpenGL", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window!" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+  setupShaders();
+  setupGeometry();
 
-	glfwMakeContextCurrent(window);
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop_arg(draw, nullptr, 0, 1);
+#else
+  while (!glEnv.shouldClose()) {
+    draw();
+    glEnv.endOfFrame();
+  }
+#endif
 
-	const GLenum err(glewInit());
-	if (err != GLEW_OK) {
-		std::stringstream s;
-		s << "Failed to init GLEW " << glewGetErrorString(err) << std::endl;
-		glfwTerminate();
-		throw GLException(s.str());
-	}
-
-	// set the opengl viewport to match the window resolution
-  int width, height;
-  glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
-	glfwSetWindowSizeCallback(window, framebuffer_size_callback);
-
-	init();
-	setupShaders();
-	setupGeometry();
-
-	// main loop
-	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
-		draw();
-		glfwPollEvents();
-	}
-
-  glfwTerminate();
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
-
